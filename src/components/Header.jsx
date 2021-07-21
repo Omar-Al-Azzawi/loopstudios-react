75fc4b77ea4ba41813b115712b9d2b24fd9e551c
Omar Al-Azzawi <alazzawiomar01@gmail.com>
2021-07-22 00:06:54 +0300

    working on nav toggle

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 import logo from "../images/logo.svg"
 
 function Header() {
+    const showNav = () => {
+        const menuBtn = document.querySelector('.menu-btn');
+        const navbar = document.querySelector('nav');
+
+        menuBtn.addEventListener('click', () => {
+            navbar.classList.toggle('open')
+            menuBtn.classList.toggle('rotate')
+        })
+    }
     return (
         <>
             <header className="header">
@@ -9,6 +18,9 @@ function Header() {
                     <img src={logo} alt="LoopStudios" />
                 </div>
                 <nav>
+                <div className="nav-logo">
+                  <img src={logo} />
+                </div>
                     <ul>
                         <li><button>About</button></li>
                         <li><button>Careers</button></li>
@@ -17,7 +29,7 @@ function Header() {
                         <li><button>Support</button></li>
                     </ul>
                 </nav>
-                <div className="menu-btn">
+                <div className="menu-btn" onClick={() => showNav()}>
                     <div className="bar bar-1"></div>
                     <div className="bar bar-2"></div>
                     <div className="bar bar-3"></div>
